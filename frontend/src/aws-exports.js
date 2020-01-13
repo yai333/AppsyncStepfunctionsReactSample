10b0b8d698bf33de27b38aa3b12830ef46bc899b
YI AI <yi.ai@neaminational.org.au>
2020-01-13 21:07:51 +1100

    remove unnecessary comment

--- frontend/src/aws-exports.js
+++ frontend/src/aws-exports.js
@@ -1,7 +1,7 @@
 const AppSyncConfig = {
-  apiKey: "da2-in23z3dg2zdghmofh5iblucvd4",
+  apiKey: "API_KEY",
   graphqlEndpoint:
-    "https://ni7s4hxpovfzbgyxwcowaipp7i.appsync-api.ap-southeast-2.amazonaws.com/graphql",
-  region: "ap-southeast-2"
+    "YOUR_APPSYNC_ENDPOINT",
+  region: "YOUR_REGION"
 };
 export default AppSyncConfig;
